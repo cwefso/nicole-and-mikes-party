35531c452664972866ae1e637e7285c7e1bf0c69
Charles Wefso <61127453+cwefso@users.noreply.github.com>
2025-06-08 21:56:18 -0600

    change rsvp deadline

--- app/content.js
+++ app/content.js
@@ -8,11 +8,11 @@ export const content = {
     is arrive, eat, drink, and make merry. Admission to the Botanic
     Gardens is included with your invite, so you can always wander
     off and enjoy the flowers if you get bored with us! Please RSVP
-    by June 19th, 2025, and please list any dietary or accommodation
+    by July 1st, 2025, and please list any dietary or accommodation
     needs. We will send out updates with more info as we get closer
     to the day.`,
     note: 'We kindly ask you leave anyone under 15 at home. We can provide a sitter if need be, just let us know!',
-    rsvpDeadline: "June 19th, 2025",
+    rsvpDeadline: "July 1st, 2025",
     contactEmail: "nicoleandmikesparty@gmail.com",
     secondEventDay: "July 20th: The Movie",
     secondEvent: "You are also welcome to join us at the SIE Film Center  (2510 E Colfax Ave, Denver, CO 80206)  for a screening of The Princess Bride - especially if you are unable to attend on Saturday!  The movie starts at 12:00!  This is an all ages event, so feel free to bring the kids."
